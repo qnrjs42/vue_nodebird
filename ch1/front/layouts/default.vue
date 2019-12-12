a34f70c1eb83cfe922cb36b8c304f5b873ddb42a
BooGeon <qnrjs42@gmail.com>
2019-12-13 06:21:32 +0900

    폼 검증, eslint 도입

--- ch1/front/layouts/default.vue
+++ ch1/front/layouts/default.vue
@@ -1,5 +1,4 @@
 <template>
-  <no-ssr>
     <v-app>
       <nav>
         <v-toolbar dark color="green">
@@ -19,26 +18,25 @@
           </v-toolbar-items>
         </v-toolbar>
       </nav>
-      <v-row>
+
+      <v-row no-gutters>
         <v-col col="12" md="4">
-          로그인창
+          <login-form />
         </v-col>
         <v-col col="12" md="8">
           <nuxt />
         </v-col>
       </v-row>
     </v-app>
-  </no-ssr>
-
 </template>
 
 
 <script>
-  import NoSSR from 'vue-no-ssr'
+  import LoginForm from '~/components/LoginForm'; // '~'는 루트라고 봐도 된다
 
   export default {
     components: {
-      'no-ssr': NoSSR
+      LoginForm,
     }
   }
 </script>
@@ -50,122 +48,3 @@ a {
   color: inherit;
 }
 </style>
-
-
-<!-- <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
-</template>
-
-<script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
-</script> -->
---
+++ ch1/front/pages/signup.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <v-container>
+      <v-card>
+        <v-container>
+          <v-subheader>회원가입</v-subheader>
+          <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+            <v-text-field
+              v-model="email"
+              label="이메일"
+              type="email"
+              :rules="emailRules"
+              requred
+             />
+             <v-text-field
+               v-model="password"
+               label="비밀번호"
+               type="password"
+               :rules="passwordRules"
+               requred
+              />
+              <v-text-field
+                v-model="passwordCheck"
+                label="비밀번호확인"
+                type="password"
+                :rules="passwordCheckRules"
+                requred
+               />
+               <v-text-field
+                 v-model="nickname"
+                 label="닉네임"
+                 type="nickname"
+                 :rules="nicknameRules"
+                 requred
+                />
+                <v-checkbox
+                v-model="terms"
+                required
+                :rules="[v => !!v || '약관에 동의해야 합니다.']"
+                label="제로초 말을 잘 들을 것을 약속합니다!"
+                />
+                <v-btn color="green" type="submit">가입하기</v-btn>
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    components: {
+
+    },
+    data() {
+      return {
+        valid: false, // 밑에 조건들이 다 들어갔는지
+        email: '',
+        password: '',
+        passwordCheck: '',
+        nickname: '',
+        terms: false, // 약관동의
+        emailRules: [
+          v => !!v || '이메일은 필수입니다.',
+          v => /.+@./.test(v) || '이메일이 유효하지 않습니다.',
+        ],
+        nicknameRules: [
+          v => !!v || '닉네임은 필수입니다.',
+        ],
+        passwordRules: [
+          v => !!v || '비밀번호는 필수입니다.',
+        ],
+        passwordCheckRules: [
+          v => !!v || '비밀번호 확인은은 필수입니다.',
+          v => v === this.password || '비밀번호가 일치하지 않습니다.',
+        ],
+      }
+    },
+    methods: {
+      onSubmitForm() {
+        if(this.$refs.form.validate()) {
+          alert('회원가입 시도!');
+        } else {
+          alert('폼이 유효하지 않습니다.');
+        }
+        console.log(this.valid);
+      }
+    },
+
+    head() {
+      return {
+        title: '회원가입',
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
