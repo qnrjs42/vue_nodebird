61b5219d1651b0131b45e4254630be84f73afab4
BooGeon <qnrjs42@gmail.com>
2019-12-13 08:32:48 +0900

    chapter 2. Vuex로 데이터 관리하기 준비

---
+++ ch2/front/components/PostCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card style="margin-bottom: 20px">
+      <v-image />
+      <v-card-text>
+        <div>
+          <h3>zecho</h3>
+          <div>안녕하신가?</div>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="orange"><v-icon>mdi-twitter-retweet</v-icon></v-btn>
+        <v-btn text color="orange"><v-icon>mdi-heart-outline</v-icon></v-btn>
+        <v-btn text color="orange"><v-icon>mdi-comment-outline</v-icon></v-btn>
+        <v-btn text color="orange"><v-icon>mdi-dots-horizontal</v-icon></v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+
+  export default {
+    components: {
+
+    },
+    data() {
+      return {
+        name: 'Nuxt.js',
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
---
+++ ch2/front/layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-app>
+      <nav>
+        <v-toolbar dark color="green">
+          <v-toolbar-title>
+            <nuxt-link to="/">NodeBird</nuxt-link>
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-text-field label="검색" hide-details prepend-icon="mdi-magnify" :style="{ display: 'flex', alignItems: 'center '}" />
+            <v-btn text nuxt to ="/profile" :style="{ display: 'flex', alignItems: 'center '}">
+              <div>프로필</div>
+            </v-btn>
+            <v-btn text nuxt to ="/signup" :style="{ display: 'flex', alignItems: 'center '}">
+              <div>회원가입</div>
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+      </nav>
+
+      <v-row no-gutters>
+        <v-col col="12" md="4">
+          <login-form />
+        </v-col>
+        <v-col col="12" md="8">
+          <nuxt />
+        </v-col>
+      </v-row>
+    </v-app>
+</template>
+
+
+<script>
+  import LoginForm from '~/components/LoginForm'; // '~'는 루트라고 봐도 된다
+
+  export default {
+    components: {
+      LoginForm,
+    }
+  }
+</script>
+
+<style scoped>
+a {
+  display: inline-block;
+  text-decoration: none;
+  color: inherit;
+}
+</style>
