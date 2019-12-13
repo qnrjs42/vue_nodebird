a9cec6838368ec7c796f03c8bf5142e8ebe82e55
BooGeon <qnrjs42@gmail.com>
2019-12-13 09:06:13 +0900

    vue actions 기능 준비 (서버에 회원가입 요청 등등)

--- ch2/front/store/users.js
+++ ch2/front/store/users.js
@@ -1,9 +1,22 @@
 export const state = () => ({
-  hello: 'vuex',
+  me: null,
 });
 
-export const mutations = ({
-  bye(state) {
-    state.hello = 'goodbye';
+export const mutations = ({ // 동기적 작업
+  setMe(state, payload) {
+    state.me = payload;
+
   }
 });
+
+export const actions = { // 비동기적 작업
+  signUp(context, payload) {
+
+  },
+  logIn(context, payload) {
+
+  },
+  logOut(context, payload) {
+
+  },
+}
