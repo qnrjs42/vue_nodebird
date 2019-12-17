b87939bedb0a7b3693ad36f5d7abbdeefd974d79
BooGeon <qnrjs42@gmail.com>
2019-12-18 05:04:10 +0900

    fetch와 인피니트 스크롤링 기능 추가

--- ch2/front/pages/index.vue
+++ ch2/front/pages/index.vue
@@ -17,20 +17,49 @@
       PostCard,
       PostForm,
     },
+
     data() {
       return {
         name: 'Nuxt.js',
       }
     },
+
     computed: {
       me() {
         return this.$store.state.users.me;
       },
       mainPosts() {
         return this.$store.state.posts.mainPosts;
+      },
+      hasMorePost() {
+        return this.$store.state.posts.hasMorePost;
       }
     },
-    
+
+    fetch({ store }) { // 컴포넌트가 화면에 보여주기 전에
+      store.dispatch('posts/loadPosts');
+    },
+
+    mounted() { // created()는 window에 접근 못함
+      window.addEventListener('scroll', this.onScroll);
+    },
+
+    beforeDestory() {
+      window.removeEventListener('scroll', this.onScroll);
+    },
+
+    methods: {
+      onScroll() {
+        if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+          if(this.hasMorePost) {
+            this.$store.dispatch('posts/loadPosts');
+
+          }
+        }
+      }
+    },
+
+
   }
 </script>
 
